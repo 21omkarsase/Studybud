fcab373abfe52b0680dde20ab09216467454c9ac
omkar sase <saseomkar214@gmail.com>
2023-12-04 11:06:56 +0530

    views and templates

--- base/views.py
+++ base/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+# from django.http import HttpResponse
 
 # Create your views here.
+
+rooms = [
+    {'id' : 0, 'name' : 'Lets learn python!'},
+    {'id' : 1, 'name' : 'Lets learn django!'},
+    {'id' : 2, 'name' : 'Lets learn scrapy!'},
+    {'id' : 3, 'name' : 'Lets learn aws!'},
+]
+
+def home(req):
+    # return HttpResponse("Welcome To Home")
+    context = {'rooms' : rooms}
+    return render(req, 'base/home.html', context)
+
+def room(req, id : str):
+    room = None
+    
+    for r in rooms:
+        if r["id"] == int(id):
+            room = r
+            
+    context = {'room' : room}
+    return render(req, 'base/room.html', context)
---
+++ notes.md
@@ -0,0 +1,33 @@
+### Install django
+
+    pip install django
+
+### Create Django Project
+
+    django-admin startproject <project_name>
+
+### Start Django Server
+
+    python manage.py runserver
+
+### Create New App
+
+    python manage.py startapp <app_name>
+
+### Add Template Folder In Base Directory
+
+    settings.py -> templates -> 'DIRS': [
+                   BASE_DIR / 'templates'
+               ],
+
+### creating templates in app folder
+
+    templates -> base -> home.html
+                         room.html
+
+    make changes in views routes
+        return render(request, '<app_name>/room.html')
+
+### Applying Default Migrations
+
+    python manage.py migrate
